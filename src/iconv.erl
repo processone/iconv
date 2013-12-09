cf8e73d2d3a0fd8d6a9269c23807c74ed2ac9edb
Evgeniy Khramtsov <ekhramtsov@process-one.net>
2013-12-09 12:44:09 +1000

    Improve DLL path detection

--- src/iconv.erl
+++ src/iconv.erl
@@ -59,17 +59,9 @@ convert(_From, _To, _String) ->
 %%% Internal functions
 %%%===================================================================
 get_so_path() ->
-    case os:getenv("EJABBERD_SO_PATH") of
-        false ->
-            case code:priv_dir(p1_iconv) of
-                {error, _} ->
-                    filename:join(["priv", "lib"]);
-                Path ->
-                    filename:join([Path, "lib"])
-            end;
-        Path ->
-            Path
-    end.
+    EbinDir = filename:dirname(code:which(?MODULE)),
+    AppDir = filename:dirname(EbinDir),
+    filename:join([AppDir, "priv", "lib"]).
 
 %%%===================================================================
 %%% Unit tests
