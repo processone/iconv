178e7bb1a3b6e443fc30a5bd834a9c9e6104ca18
Mickael Remond <mremond@process-one.net>
2016-01-21 11:21:20 +0100

    Update copyright year to 2016

--- src/iconv.erl
+++ src/iconv.erl
@@ -5,7 +5,7 @@
 %%% Created : 16 Feb 2003 by Alexey Shchepin <alexey@process-one.net>
 %%%
 %%%
-%%% eiconv, Copyright (C) 2002-2015   ProcessOne
+%%% eiconv, Copyright (C) 2002-2016   ProcessOne
 %%%
 %%% This program is free software; you can redistribute it and/or
 %%% modify it under the terms of the GNU General Public License as
--- src/iconv_sup.erl
+++ src/iconv_sup.erl
@@ -5,7 +5,7 @@
 %%% Created : 4 Apr 2013 by Evgeniy Khramtsov <ekhramtsov@process-one.net>
 %%%
 %%%
-%%% eiconv, Copyright (C) 2002-2015   ProcessOne
+%%% eiconv, Copyright (C) 2002-2016   ProcessOne
 %%%
 %%% This program is free software; you can redistribute it and/or
 %%% modify it under the terms of the GNU General Public License as
